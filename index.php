337e22b19b52b6794f33f143be970ba244544f96
Thunderbird93 <antircristian@gmail.com>
2022-12-07 13:42:20 +0100

    [add] costruttore

--- index.php
+++ index.php
@@ -6,4 +6,16 @@ class Movie{
     public $genre;
     public $lenght;
     public $poster;
+
+    function __construct($_title, $_director)
+    {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->protagonist = $_protagonist;
+        $this->genre = $_genre;
+        $this->lenght = $_lenght;
+        $this->poster = $_poster;
+    }
 }
+
+
